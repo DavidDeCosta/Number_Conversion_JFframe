bb21a29a3eb4a4cf49c1ca53048ceb18cfdae153
David DeCosta <DavidDeCosta12@gmail.com>
2022-09-05 19:44:35 -0400

    big changes

--- MyFrame.java
+++ MyFrame.java
@@ -5,33 +5,29 @@ import java.awt.*;                                                         // fo
 import java.awt.event.*;
 
 public class MyFrame extends JFrame 
-                    implements ChangeListener, ActionListener
+                    implements ChangeListener, ActionListener, DocumentListener
 {
 //=====================================DATA MEMBERS========================================================================
-    JFrame frame;
+
     Dimension screenSize;
     Toolkit toolkit;
-    SpinnerModel spinnerModel;
 
     JPanel panel_1;
     JPanel panel_2;
     JPanel panel_3;
     JPanel panel_4;
     JPanel panel_5;
-    JPanel panel_6;
-    JPanel panel_7;
 
     JLabel label_1;
     JLabel label_2;
 
-    JButton button_1;
-    JButton button_2;
+    JButton exitButton;
 
     JTextField textField_1;
     JTextField textField_2;
 
     JSpinner spinner;
-    SpinnerModel spinerModel;
+    SpinnerModel spinnerModel;
     JLabel spinnerTitleLabel;
     JLabel spinnerValueLabel;
     
@@ -39,73 +35,67 @@ public class MyFrame extends JFrame
 //====================================== CONSTRUCTOR=========================================================================
     MyFrame()
     {
-
-        frame = new JFrame();
-
         toolkit = Toolkit.getDefaultToolkit();                            // used to help get the users screen size
         screenSize = toolkit.getScreenSize();                             //get the users screen size
-        frame.setSize(screenSize.width/3, screenSize.height/3);           // makes JFrame 1/3 the users screensize
-        frame.setLocationRelativeTo(null);                             // window is placed in the center of screen
-        frame.setVisible(true);                                        // makes the window visible
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);            //when close frame the program stops
-        frame.setTitle("Project 1 Number Conversion");
+        setSize(screenSize.width/3, screenSize.height/3);           // makes JFrame 1/3 the users screensize
+        setLocationRelativeTo(null);                             // window is placed in the center of screen
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);            //when close frame the program stops
+        setTitle("Project 1 Number Conversion");
 
 
         addPanels();
         addComponents();
-        
+        setVisible(true);                                       //had to make visable once everything was added
         
     }
     //===================================METHODS=============================================================================
 
-    void addPanels()
+    public void addPanels()
     {
         
         panel_1 = new JPanel();
         panel_1.setBackground(Color.GREEN);
         panel_1.setPreferredSize(new Dimension(50,50));
         
-        frame.add(panel_1,BorderLayout.NORTH);
+        add(panel_1,BorderLayout.NORTH);
 
         panel_2 = new JPanel();
         panel_2.setBackground(Color.RED);
         panel_2.setPreferredSize(new Dimension(50,50));
         
-        frame.add(panel_2,BorderLayout.SOUTH);
+        add(panel_2,BorderLayout.SOUTH);
 
         panel_3 = new JPanel();
         panel_3.setBackground(Color.YELLOW);
         panel_3.setPreferredSize(new Dimension(100,50));
         
-        frame.add(panel_3,BorderLayout.EAST);
+        add(panel_3,BorderLayout.EAST);
 
         panel_4 = new JPanel();
         panel_4.setBackground(Color.BLUE);
         panel_4.setPreferredSize(new Dimension(50,50));
         
-        frame.add(panel_4,BorderLayout.WEST);
+        add(panel_4,BorderLayout.WEST);
 
         panel_5 = new JPanel();
         panel_5.setBackground(Color.PINK);
         panel_5.setPreferredSize(new Dimension(50,50));
         
-        frame.add(panel_5,BorderLayout.CENTER);
-
+        add(panel_5,BorderLayout.CENTER);
     }
 
-    void addComponents()
-    {
-//        panel_6 = new JPanel();
-//        panel_3.setLayout(new BorderLayout()); //makes it so we can add components to 'N''S''E''W'inside panel_3                                            
+    public void addComponents()
+    {                                           
 
         spinnerModel = new SpinnerNumberModel(2,2,20,1);
-        spinnerModel.addChangeListener(this);
+        spinnerModel.addChangeListener(this);                                 // listens to see if you click the up or down arrow
         spinner = new JSpinner(spinnerModel);
-
         spinnerTitleLabel = new JLabel("The Spinner");
         spinnerTitleLabel.setFont(new Font("Times Roman", Font.PLAIN,18));
-
         spinnerValueLabel = new JLabel("2");
+
+
+
         panel_3.add(spinnerTitleLabel,BorderLayout.NORTH);
         panel_3.add(spinner,BorderLayout.CENTER);
 
@@ -117,8 +107,10 @@ public class MyFrame extends JFrame
         textField_1.setBackground(Color.WHITE);
         textField_1.setForeground(Color.RED);;
 
-        textField_1.addActionListener(this);
+        textField_1.getDocument().addDocumentListener(this);
 
+        
+        panel_5.setLayout(new GridLayout(2,1));                //set so the input box is ontop of the output box
         panel_5.add(label_1);
         panel_5.add(textField_1);
 
@@ -126,13 +118,32 @@ public class MyFrame extends JFrame
         textField_2 = new JTextField();
         textField_2.setPreferredSize(new Dimension(150,50));        //sets the dimensions of output box
         textField_2.setFont(new Font("Times Roman", Font.PLAIN,18));
-        textField_2.setBackground(Color.BLACK);        //background of output box is black
-        textField_2.setForeground(Color.GREEN);        //output text will be green
-        textField_2.setEditable(false);             //output field is not editable
+        textField_2.setBackground(Color.BLACK);                                  //background of output box is black
+        textField_2.setForeground(Color.GREEN);                                  //output text will be green
+        textField_2.setEditable(false);                                        //output field is not editable
 
-        panel_5.setLayout(new GridLayout(2,1));                //set so the input box is ontop of the output box
         panel_5.add(label_2);                                             
         panel_5.add(textField_2);
+
+        exitButton = new JButton("Exit");
+        exitButton.addActionListener(this);                   // listens for the button to be clicked and send it to actionperformed
+        panel_2.add(exitButton);
+    }
+
+
+    /*
+    In: Takes in an integer
+    Out: Returns the String version of the integer
+     */
+    String convertNumber(String originalNum, int rad){
+
+        String convertedNum;
+        int i;
+        i =  Integer.parseInt(originalNum);
+        convertedNum= Integer.toString(i,rad);
+
+
+        return convertedNum;
     }
 
     public void stateChanged(ChangeEvent ce)
@@ -142,15 +153,78 @@ public class MyFrame extends JFrame
         n = (Integer) spinner.getModel().getValue();
 
         spinnerValueLabel.setText("" + n);
-    }
 
-    public void actionPerformed(ActionEvent e)
-    {
-        if(e.getSource()== textField_1)
+
+        String convertedNum;                               
+        String str1;
+        str1 = textField_1.getText().trim();                              //str1 hold the text in textfield1
+        if(textField_1.getText().trim().isEmpty())                        //if textfield1 is empty set texfield2 to empty
         {
-            String text = textField_1.getText();
-            textField_2.setText(text);
+            textField_2.setText("");
+        }
+        else
+        {
+//        int k;
+//        k = (Integer) spinner.getModel().getValue();                // n hold the value of the spinner
+        convertedNum = convertNumber(str1, n);                      // convertedNum is the new string number
+        textField_2.setText(convertedNum);                          // puts the convertedNum into the textfield2
         }
     }
 
+
+     public void actionPerformed(ActionEvent e)
+    {
+        this.dispose();                            //closes the Jframe
+
+    }
+
+    @Override
+    public void insertUpdate(DocumentEvent e) {
+        
+        String convertedNum;
+        String str1;
+        str1 = textField_1.getText().trim();
+        int n;
+
+        n = (Integer) spinner.getModel().getValue();
+        convertedNum = convertNumber(str1, n);
+        textField_2.setText(convertedNum);
+
+
+    }
+
+    @Override
+    public void removeUpdate(DocumentEvent e) {
+        
+        String convertedNum;                               
+        String str1;
+        str1 = textField_1.getText().trim();                              //str1 hold the text in textfield1
+        if(textField_1.getText().trim().isEmpty())                        //if textfield1 is empty set texfield2 to empty
+        {
+            textField_2.setText("");
+        }
+        else
+        {
+        int n;
+
+        n = (Integer) spinner.getModel().getValue();                // n hold the value of the spinner
+        convertedNum = convertNumber(str1, n);                      // convertedNum is the new string number
+        textField_2.setText(convertedNum);                          // puts the convertedNum into the textfield2
+        }
+    }
+
+    @Override
+    public void changedUpdate(DocumentEvent e) {
+        
+
+        String convertedNum;
+        String str1;
+        str1 = textField_1.getText().trim();
+        int n;
+
+        n = (Integer) spinner.getModel().getValue();
+        convertedNum = convertNumber(str1, n);
+        textField_2.setText(convertedNum);
+        
+    }
 }
